c06e3bb85d27dd8a66a71a082aad6a99d4912694
Andreas Schuh <andreas.schuh.84@gmail.com>
2016-07-26 15:08:19 +0100

    enh: Add deleter type to UniquePtr template [Common]

--- Modules/Common/include/mirtk/SmartPtr.h
+++ Modules/Common/include/mirtk/SmartPtr.h
@@ -28,8 +28,8 @@ namespace mirtk {
 using std::dynamic_pointer_cast;
 
 
-template <class T>
-using UniquePtr = std::unique_ptr<T>;
+template <class T, class D = std::default_delete<T> >
+using UniquePtr = std::unique_ptr<T, D>;
 
 template <class T>
 using SharedPtr = std::shared_ptr<T>;
